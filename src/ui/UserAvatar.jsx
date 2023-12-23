dd57bf8d58f84fb2023d86d5bec0017ac306ceb1
mersad98 <mersadrezazadeh@gmail.com>
2023-12-23 12:28:57 +0330

    fix avatar image path

--- src/ui/UserAvatar.jsx
+++ src/ui/UserAvatar.jsx
@@ -8,7 +8,7 @@ function UserAvatar() {
   return (
     <div className="flex items-center gap-3 text-sm font-medium text-gray-600 dark:text-gray-300">
       <img
-        src={avatar || "../public/default-user.jpg"}
+        src={avatar || "/default-user.jpg"}
         alt={`تصویر ${fullName}`}
         className="block aspect-square w-9 rounded-full object-cover object-center outline-2 outline-gray-100 dark:outline-gray-800"
       />
